313123833921d0fc19ac9471facce8728ad87a32
Darrell Plant <329873+darrell-plant@users.noreply.github.com>
2021-11-25 15:00:02 +0000

    VTCCA-0 darrell 2 (#2)
    * add Hello to Darrell
    
    * save
    
    * save

--- src/main/scala/rebase/Hello.scala
+++ src/main/scala/rebase/Hello.scala
@@ -10,6 +10,8 @@ object Hello extends App {
 
   println("Hello, Jack!")
 
+  println("Hello, Darrell!")
+
   println("How to git rebase...")
 
   println(
@@ -18,9 +20,20 @@ object Hello extends App {
       | git checkout main && git pull && git checkout -
       | (alias 'gpm' for me)
       |
+      |Then make sure you have committed your worl on this branch
+      |
       |Then rebase interactively:
       | git rebase -i main
       | (alias 'grim' for me)
+      |
+      |Next, typically you save the rebase commit message as is
+      |containing the commits current on your branch (more advanced options available)
+      |  "pick <commit_id> <commit_msg"
+      |[save]
+      |
+      |If all goes well, then rebase is complete.
+      |
+      |There may be merge conflicts though
       |
       |
       |""".stripMargin
